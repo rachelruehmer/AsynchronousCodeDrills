a05e35a998ced203742cfa6f35ee767aab083624
IB_Student <student@InnovateBhams-MacBook-Pro.local>
2020-02-12 16:49:47 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,41 @@
+
+// console.log('Here we go');
+// setTimeout(() => {
+//     console.log('Hello World!');
+//    }, 2000);
+
+// function getWords();
+
+//    console.log('Hello');
+
+
+//    setTimeout(() => {
+//     console.log('How');
+//    }, 1000);
+
+//    setTimeout(() => {
+//     console.log('are');
+//    }, 2000);
+
+//    setTimeout(() => {
+//     console.log('you?');
+//    }, 3000); 
+
+// let countDown = (num, callback) => {
+//     setTimeout(() => {
+//         if (num > 0) {
+//             console.log(num);
+//             countDown(num - 1, callback);
+//         } else {
+//             callback();
+//         }
+//         }, 1000);
+// };
+// const done = () => {
+//     console.log("Job's Done");
+// };
+// countDown(5, done);
+
+lunchTime = true; {
+
+}
